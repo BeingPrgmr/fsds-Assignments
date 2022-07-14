726292db16d8292fb37e3cdde3f0c4a304cb72f9
BeingPrgmr <talharocks33@gmail.com>
2022-07-14 23:07:20 +0530

    Add files via upload

---
+++ assgn1.py
@@ -0,0 +1,53 @@
+#1. In the below elements which of them are values or an expression? eg:- values can be
+#integer or string and expressions will be mathematical operators.
+
+# * - Operator
+# "hello" - value
+# -87.8 - value
+# -  expression
+# / - expression
+# + - expression
+# 6 - value
+
+
+
+# What is the difference between string and variable?
+
+# String - String is a datatype
+# Variable - Variable is a container for storing data
+# of different types
+
+
+# Describe three data types
+
+# int:- Integer numbers
+# string:- anything enclosed withing single double or triple quotes
+# boolean:- True or False
+
+#6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+
+#22
+
+#7. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' *3
+
+
+# No values because their is no print statement used
+
+
+
+#8. Why is eggs a valid variable name while 100 is invalid?
+
+#Variable name cannot start with a number
+
+
+
+
+#10. Why does this expression cause an error? How can you fix it?
+#'I have eaten' + 99 + 'burritos.'
+
+
+# You cannot concatenate string and integer together
